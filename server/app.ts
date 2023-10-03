c2b94bdbefcb3237f2df307e5f9b3db73aee41c3
Sushant Shukla <1805352@kiit.ac.in>
2023-10-03 19:54:52 +0530

    setup basic api routes

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,35 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+require("dotenv").config();
 
 export const app = express();
+
+//Body Parser
+app.use(express.json({ limit: "50mb" }));
+
+//Cookie Parser
+app.use(cookieParser());
+
+//CORS - Cross Origin Resource Sharing
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+//TESTING APIs
+app.get("/test", (req: Request, resp: Response, next: NextFunction) => {
+  resp.status(200).json({
+    success: true,
+    message: "api working",
+  });
+});
+
+
+// UNKNOWN Routes
+app.all("*", (req: Request, resp: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+});
